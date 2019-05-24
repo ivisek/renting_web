0e82abc36569b29192368304b0a0db9196af5170
ivisek <ivana.visek@gmail.com>
2019-05-24 17:07:08 +0200

    fix

--- app/controllers/startup_controller.rb
+++ app/controllers/startup_controller.rb
@@ -5,7 +5,7 @@ class StartupController < ApplicationController
 
   def register
   	details = params.except(:utf8, :authenticity_token, :commit, :controller, :action)
-  	RegisterMailer.new_guest(details).deliver if !details.blank?
+  	RegisterMailer.new_guest(details).deliver if !details.reject {|k, v| v.blank?}.blank?
   end
   
 end
