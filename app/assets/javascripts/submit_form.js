81b6396bb1378961cc9dc7670e5ac30a474f1a63
ivisek <ivana.visek@gmail.com>
2019-06-19 18:59:30 +0200

    returned to the old state

--- app/assets/javascripts/submit_form.js
+++ app/assets/javascripts/submit_form.js
@@ -1,3 +1,5 @@
+//= require jquery3
+
 $(document).ready(function(){
 	$('#submit_form').click(function(e){
 		e.preventDefault();
@@ -7,7 +9,7 @@ $(document).ready(function(){
 		    data: "registration_data[first_name]=" + $('#first_name').val() + "&registration_data[middle_name]=" + $('#middle_name').val() + "&registration_data[last_name]=" + $('#last_name').val() + "&registration_data[gender]=" + $(".form-group input[type='radio']:checked").val() + "&registration_data[birth_country]=" + $('#birth_country').val() + "&registration_data[birth_city]=" + $('#birth_city').val() + "&registration_data[birth_date]=" + $('#birth_date').val() + "&registration_data[residence_country]=" + $('#residence_country').val() + "&registration_data[residence_city]=" + $('#residence_city').val() + "&registration_data[residence_address]=" + $('#residence_address').val(),
 		    success: function(response){
 		    		// alert('Registration email sent!');
-		    		// $('#reset_form').click();
+		    		$('#reset_form').click();
 		    },
 		    error: function (ex) {
 	            // alert('Smth went wrong');
